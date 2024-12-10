28899a4f43f6b27dabdafd407084686a538d2937
Jaizzer <florenzjaizzer.calderon@gmail.com>
2024-12-10 14:15:01 +0800

    feat: Create initial server setup

---
+++ server/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import { getCourse } from './course.js';
+import cors from 'cors';
+
+// Initialize an Express application
+const app = express();
+
+// Use CORS middleware to allow cross-origin requests from any origin
+app.use(cors());
+
+// Set the port for the server to listen on
+const PORT = 9090;
+
+// Start the server and listen on the specified port
+app.listen(PORT, () => {
+	console.log(`Server is running at http://localhost:${PORT}`);
+});
