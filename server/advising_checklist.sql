44b2a6d3e79a29955eb2cfd0170fe157996a3f1d
Jaizzer <florenzjaizzer.calderon@gmail.com>
2024-12-10 12:00:21 +0800

    style: Update cases for consistency

--- server/advising_checklist.sql
+++ server/advising_checklist.sql
@@ -11,28 +11,28 @@ CREATE TABLE `Adviser` (
 );
 
 CREATE TABLE `Course` (
-  `CourseID` VARCHAR(200) NOT NULL,
+  `CourseId` VARCHAR(200) NOT NULL,
   `CourseDescription` VARCHAR(200) DEFAULT NULL,
   `Units` tinyint DEFAULT NULL,
   `CourseComponents` VARCHAR(200) DEFAULT NULL,
   `College` VARCHAR(200) DEFAULT NULL,
   `Department` VARCHAR(200) DEFAULT NULL,
   `GradingBasis` VARCHAR(200) DEFAULT NULL,
-  PRIMARY KEY (`CourseID`)
+  PRIMARY KEY (`CourseId`)
 );
 
 CREATE TABLE `CourseCorequisite` (
-    `CourseID` VARCHAR(200) NOT NULL,
+    `CourseId` VARCHAR(200) NOT NULL,
     `Corequisite` VARCHAR(200) NOT NULL,
     FOREIGN KEY (`CourseId`) REFERENCES `Course`(`CourseId`) ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY (`Corequisite`) REFERENCES `Course`(`CourseId`) ON DELETE CASCADE ON UPDATE CASCADE
 )
 
 CREATE TABLE `CoursePrerequisite` (
-  `CourseID` VARCHAR(200) NOT NULL,
+  `CourseId` VARCHAR(200) NOT NULL,
   `Prerequisite` VARCHAR(200) DEFAULT NULL,
-  FOREIGN KEY (`CourseID`) REFERENCES `Course`(`CourseID`) ON DELETE CASCADE ON UPDATE CASCADE,
-  FOREIGN KEY (`Prerequisite`) REFERENCES `Course`(`CourseID`) ON DELETE CASCADE ON UPDATE CASCADE
+  FOREIGN KEY (`CourseId`) REFERENCES `Course`(`CourseId`) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (`Prerequisite`) REFERENCES `Course`(`CourseId`) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE `ProgramChecklist` (
@@ -43,7 +43,7 @@ CREATE TABLE `ProgramChecklist` (
   `PrescribedSemester` VARCHAR(200) DEFAULT NULL,
   `DateLastUpdated` DATE NOT NULL,
   `TimeLastUpdated` TIME NOT NULL,
-  PRIMARY KEY (`StudentProgram`, `CourseID`), 
+  PRIMARY KEY (`StudentProgram`, `CourseId`), 
   FOREIGN KEY (`CourseId`) REFERENCES `Course`(`CourseId`) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
@@ -62,7 +62,7 @@ CREATE TABLE `Student` (
 
 CREATE TABLE `StudentCourseList` (
   `StudentNumber` VARCHAR(200) NOT NULL,
-  `CourseID` VARCHAR(200) DEFAULT NULL,
+  `CourseId` VARCHAR(200) DEFAULT NULL,
   `CourseStatus` text,
   `Grade` float DEFAULT NULL,
   `StandingTaken` VARCHAR(200) DEFAULT NULL,
@@ -71,5 +71,5 @@ CREATE TABLE `StudentCourseList` (
   `DateSubmitted` date DEFAULT NULL,
   `TimeSubmitted` time DEFAULT NULL,
   FOREIGN KEY (`StudentNumber`) REFERENCES `Student`(`StudentNumber`) ON DELETE CASCADE ON UPDATE CASCADE,
-  FOREIGN KEY (`CourseID`) REFERENCES `Course`(`CourseID`) ON DELETE CASCADE ON UPDATE CASCADE
+  FOREIGN KEY (`CourseId`) REFERENCES `Course`(`CourseId`) ON DELETE CASCADE ON UPDATE CASCADE
 );
