0bd1c83ca1ca462a12d92653172470987cea9dd9
Jaizzer <florenzjaizzer.calderon@gmail.com>
2024-12-10 15:18:45 +0800

    feat: Add parameter to require the student program before requesting course or courses

--- server/server.js
+++ server/server.js
@@ -12,9 +12,21 @@ app.use(cors());
 const PORT = 9090;
 
 // Create courses route
-app.get('/courses', async (request, response) => {
-	// Get the courses
-	const { courses } = await getCourse();
+app.get('/courses/:studentProgram', async (request, response) => {
+    console.log("studentProgram")
+
+	// Get the student program from the URL parameter and decode it in case it contains spaces
+	const studentProgram = decodeURIComponent(request.params.studentProgram);
+
+	// Get the courses based on the student program
+	const { courses, error } = await getCourse(null, studentProgram);
+
+	// Check if courses were fetched successfully
+	if (error) {
+		return response.status(400).json({ success: false, error });
+	}
+
+	// Return the courses as a JSON response
 	response.json(courses);
 });
 
